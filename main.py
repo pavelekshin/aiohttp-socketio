8602d545f95beb6e80482e9f8617a078b4586d39
Pavel Ekshin <pavel.ekshine@gmail.com>
2024-05-01 12:18:07 +0300

    add run func

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import logging
 from aiohttp import web
 from app import init_app
 
-if __name__ == '__main__':
+
+def run():
     app = init_app()
     logger = logging.getLogger()
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
     web.run_app(app, port=8080, shutdown_timeout=3, access_log=logger, loop=loop)
+
+
+if __name__ == '__main__':
+    run()
