78dbebe636e81a63f51e21e5266bbda938e74e9a
Pavel Ekshin <pavel.ekshine@gmail.com>
2024-08-01 00:17:38 +0300

    changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,12 @@
+import asyncio
+
 import pytest
+import pytest_asyncio
+import socketio
+from aiohttp import web
 from pytest_asyncio import is_async_test
+
+from src.app import init_app
 
 
 # https://pytest-asyncio.readthedocs.io/en/latest/how-to-guides/run_session_tests_in_same_loop.html
@@ -8,3 +15,98 @@ def pytest_collection_modifyitems(items):
     session_scope_marker = pytest.mark.asyncio(scope="session")
     for async_test in pytest_asyncio_tests:
         async_test.add_marker(session_scope_marker, append=False)
+
+
+@pytest_asyncio.fixture(scope="session")
+async def server():
+    app = await init_app()
+    runner = web.AppRunner(app, shutdown_timeout=3)
+    await runner.setup()
+    site = web.TCPSite(runner, port=8080)
+    await site.start()
+    yield site
+    await asyncio.sleep(1)
+    for task in asyncio.all_tasks():
+        task.cancel()
+    await runner.cleanup()
+
+
+@pytest_asyncio.fixture(scope="session", name="sio")
+async def client(server):
+    sio = socketio.AsyncClient(logger=True, engineio_logger=True)
+    init_chat(sio)
+    init_riddle(sio)
+    init_trivia(sio)
+    yield sio
+    await sio.disconnect()
+
+
+@pytest_asyncio.fixture(scope="session", name="conn")
+async def conn(sio):
+    try:
+        await sio.connect(url="http://127.0.0.1:8080")
+        yield sio
+        await sio.disconnect()
+    except socketio.exceptions.ConnectionError as err:
+        print(f"Error: {err}")
+        await sio.disconnect()
+
+
+def idtype(val):
+    if not isinstance(val, str):
+        return type(val)
+
+
+EXPECTED_CHAT_DATA = []
+EXPECTED_RIDDLE_DATA = []
+EXPECTED_TRIVIA_DATA = []
+
+
+def init_chat(sio):
+    @sio.on("connect", namespace="/chat")
+    async def on_connect():
+        EXPECTED_CHAT_DATA.append("connected")
+
+    @sio.on("message", namespace="/chat")
+    async def on_message(data):
+        EXPECTED_CHAT_DATA.append(data)
+
+    @sio.on("rooms", namespace="/chat")
+    async def on_rooms(data):
+        EXPECTED_CHAT_DATA.append(data)
+
+
+def init_riddle(sio):
+    @sio.on("connect", namespace="/riddle")
+    async def on_connect():
+        EXPECTED_RIDDLE_DATA.append("connected")
+
+    @sio.on("answer", namespace="/riddle")
+    async def on_answer(data):
+        EXPECTED_RIDDLE_DATA.append(data)
+
+    @sio.on("riddle", namespace="/riddle")
+    async def on_riddle(data):
+        EXPECTED_RIDDLE_DATA.append(data)
+
+    @sio.on("result", namespace="/riddle")
+    async def on_result(data):
+        EXPECTED_RIDDLE_DATA.append(data)
+
+    @sio.on("score", namespace="/riddle")
+    async def on_score(data):
+        EXPECTED_RIDDLE_DATA.append(data)
+
+
+def init_trivia(sio):
+    @sio.on("connect", namespace="/trivia")
+    async def on_connect():
+        EXPECTED_TRIVIA_DATA.append("connected")
+
+    @sio.on("topics", namespace="/trivia")
+    async def on_topics(data):
+        EXPECTED_TRIVIA_DATA.append(data)
+
+    @sio.on("game", namespace="/trivia")
+    async def on_game(data):
+        EXPECTED_TRIVIA_DATA.append(data)
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,108 +1,11 @@
-import asyncio
-
 import pytest
-import pytest_asyncio
-import socketio
-from aiohttp import web
 from socketio import AsyncClient
 
-from src.app import init_app
 from src.apps.trivia import game_container, get_answer_body
 from src.config.config_folder import get_config_folder
 from src.helper import generate_game_uuid
 from src.modules.mod import Riddle
-
-EXPECTED_CHAT_DATA = []
-EXPECTED_RIDDLE_DATA = []
-EXPECTED_TRIVIA_DATA = []
-
-
-def init_chat(sio):
-    @sio.on("connect", namespace="/chat")
-    async def on_connect():
-        EXPECTED_CHAT_DATA.append("connected")
-
-    @sio.on("message", namespace="/chat")
-    async def on_message(data):
-        EXPECTED_CHAT_DATA.append(data)
-
-    @sio.on("rooms", namespace="/chat")
-    async def on_rooms(data):
-        EXPECTED_CHAT_DATA.append(data)
-
-
-def init_riddle(sio):
-    @sio.on("connect", namespace="/riddle")
-    async def on_connect():
-        EXPECTED_RIDDLE_DATA.append("connected")
-
-    @sio.on("answer", namespace="/riddle")
-    async def on_answer(data):
-        EXPECTED_RIDDLE_DATA.append(data)
-
-    @sio.on("riddle", namespace="/riddle")
-    async def on_riddle(data):
-        EXPECTED_RIDDLE_DATA.append(data)
-
-    @sio.on("result", namespace="/riddle")
-    async def on_result(data):
-        EXPECTED_RIDDLE_DATA.append(data)
-
-    @sio.on("score", namespace="/riddle")
-    async def on_score(data):
-        EXPECTED_RIDDLE_DATA.append(data)
-
-
-def init_trivia(sio):
-    @sio.on("connect", namespace="/trivia")
-    async def on_connect():
-        EXPECTED_TRIVIA_DATA.append("connected")
-
-    @sio.on("topics", namespace="/trivia")
-    async def on_topics(data):
-        EXPECTED_TRIVIA_DATA.append(data)
-
-    @sio.on("game", namespace="/trivia")
-    async def on_game(data):
-        EXPECTED_TRIVIA_DATA.append(data)
-
-
-@pytest_asyncio.fixture(scope="session")
-async def server():
-    app = await init_app()
-    runner = web.AppRunner(app, shutdown_timeout=3)
-    await runner.setup()
-    site = web.TCPSite(runner, port=8080)
-    await site.start()
-    yield site
-    await asyncio.sleep(1)
-    await runner.cleanup()
-
-
-@pytest_asyncio.fixture(scope="session", name="sio")
-async def client(server):
-    sio = socketio.AsyncClient(logger=True, engineio_logger=True)
-    init_chat(sio)
-    init_riddle(sio)
-    init_trivia(sio)
-    yield sio
-    await sio.disconnect()
-
-
-@pytest_asyncio.fixture(scope="session", name="conn")
-async def conn(sio):
-    try:
-        await sio.connect(url="http://127.0.0.1:8080")
-        yield sio
-        await sio.disconnect()
-    except socketio.exceptions.ConnectionError as err:
-        print(f"Error: {err}")
-        await sio.disconnect()
-
-
-def idtype(val):
-    if not isinstance(val, str):
-        return type(val)
+from tests.conftest import idtype, EXPECTED_CHAT_DATA, EXPECTED_RIDDLE_DATA, EXPECTED_TRIVIA_DATA
 
 
 @pytest.mark.parametrize(
@@ -111,9 +14,9 @@ def idtype(val):
         ("connected", None, "connected"),
         ("get_rooms", {}, ["sex", "drugs", "rock'n'roll"]),
         (
-            "join",
-            {"name": "test_client", "room": "lobby"},
-            {"text": "welcome to lobby"},
+                "join",
+                {"name": "test_client", "room": "lobby"},
+                {"text": "welcome to lobby"},
         ),
     ],
     ids=idtype,
@@ -136,22 +39,22 @@ def riddle():
         ("connected", None, "connected"),
         ("next", {}, {"text": riddle().question}),
         (
-            "answer",
-            {"text": riddle().answer},
-            {
-                "riddle": riddle().question,
-                "is_correct": "true",
-                "answer": riddle().answer,
-            },
+                "answer",
+                {"text": riddle().answer},
+                {
+                    "riddle": riddle().question,
+                    "is_correct": "true",
+                    "answer": riddle().answer,
+                },
         ),
         (
-            "answer",
-            {"text": "WRONG"},
-            {
-                "riddle": riddle().question,
-                "is_correct": "false",
-                "answer": riddle().answer,
-            },
+                "answer",
+                {"text": "WRONG"},
+                {
+                    "riddle": riddle().question,
+                    "is_correct": "false",
+                    "answer": riddle().answer,
+                },
         ),
         ("score", {}, {"value": 1}),
         ("recreate", {}, {"text": riddle().question}),
@@ -173,7 +76,7 @@ def trivia_topics():
 
 
 def trivia_game():
-    topic = 5
+    topic = '5'
     trivia = game_container.get_item("topics")
     question_path = get_config_folder("trivia_questions.csv")
     trivia.load_questions(question_path)
@@ -188,7 +91,7 @@ def trivia_game():
     [
         ("connected", None, "connected"),
         ("get_topics", {}, trivia_topics()),
-        ("join_game", {"topic_pk": 5, "name": "Player_name"}, trivia_game()),
+        ("join_game", {"topic_pk": '5', "name": "Player_name"}, trivia_game()),
         # TODO: complete tests
     ],
     ids=idtype,
